2aed100b9b3ed92eb0f2060139c4c8fc9ebaa0e8
tjlee527 <tjlee527@gmail.com>
2020-05-20 10:41:39 -0700

    added helper functions

---
+++ utils/helpers.js
@@ -0,0 +1,44 @@
+import React from 'react'
+import { View, StyleSheet, AsyncStorage } from 'react-native'
+
+const ALL_DECKS_KEY = 'Flashcards:allDecks'
+
+// getDecks: return all of the decks along with their titles, questions, and answers.
+export function getDecks () {
+  return AsyncStorage.getItem(ALL_DECKS_KEY)
+    .then(JSON.parse)
+    .then((data) => {
+      console.log(data)
+      return data
+    })
+    .catch((err) => console.warn('error getting desks: ', err)
+}
+
+// getDeck: take in a single id argument and return the deck associated with that id.
+export function getDeck(id) {
+  return AsyncStorage.getItem(ALL_DECKS_KEY)
+    .then(JSON.parse)
+    .then((data) => data[id])
+    .catch((err) => console.warn('error getting desks: ', err)
+}
+
+// saveDeckTitle: take in a single title argument and add it to the decks.
+export function saveDeckTitle(title) {
+  const deck = {
+    [title]: title
+  }
+
+  return AsyncStorage.mergeItem(ALL_DECKS_KEY, deck)
+    .catch((err) => console.warn('error adding new deck: ', err))
+}
+
+// addCardToDeck: take in two arguments, title and card, and will add the card to the list of questions for the deck with the associated title.
+// card is an obj of answer & question
+export function addCardToDeck(title, card) {
+  AsyncStorage.getItem(ALL_DECKS_KEY)
+    .then(JSON.parse)
+    .then((data) => {
+      data[title].questions.push(card)
+      AsyncStorage.mergeItem(ALL_DECKS_KEY, JSON.stringify(data))
+    })
+}
