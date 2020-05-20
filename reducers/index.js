8d5d499acf865c1e65b929bcb7c0629920a8c84e
tjlee527 <tjlee527@gmail.com>
2020-05-20 10:56:40 -0700

    added actions & reducers

---
+++ reducers/index.js
@@ -0,0 +1,30 @@
+import { RECEIVE_DECKS, ADD_DECK, ADD_CARD } from '../actions'
+
+function decks ( state = {}, action) {
+  switch(action.type) {
+    case RECEIVE_DECKS :
+      return {
+        ...state,
+        ...action.decks
+      }
+
+    case ADD_DECK :
+      return {
+        ...state,
+        ...action.title
+      }
+
+    case ADD_CARD :
+      return {
+        ...state,
+        [action.title]: {
+          ...state[action.title],
+          questions: state[action.title].questions.concat([action.card])
+          }
+        }
+      }
+
+    default :
+      return state
+  }
+}
